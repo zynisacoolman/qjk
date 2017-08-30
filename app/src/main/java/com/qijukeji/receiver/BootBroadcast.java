b0e717f8aa4f750efa73aa6b3b2b247cf5f8ab97
xinpeng <xinpeng>
2017-08-30 17:43:12 +0800

    设置了应用开机自启动，优化了扫码耗电发热，修改了分享面板的背景图片，优化了订单页面华东效果，为发现页面的item增加了点击详情（暂时跳转到百度）

---
+++ app/src/main/java/com/qijukeji/receiver/BootBroadcast.java
@@ -0,0 +1,10 @@
+package com.qijukeji.receiver;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by Qiju on 2017/8/29.
+ */
+
+public class BootBroadcast extends BroadcastReceiver {
+}
---
+++ app/src/main/java/com/qijukeji/receiver/WatchService.java
@@ -0,0 +1,10 @@
+package com.qijukeji.receiver;
+
+import android.app.Service;
+
+/**
+ * Created by Qiju on 2017/8/29.
+ */
+
+public class watchService extends Service {
+}
